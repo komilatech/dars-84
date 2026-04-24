5b6b6d11b93a4adf6b8059160e63baed6f6e6ab5
komilatech <komilatech12@gmail.com>
2026-04-24 14:04:26 +0500

    script.js qo'shildi

---
+++ script.js
@@ -0,0 +1,7 @@
+
+console.log("Dars 84");
+function test() {
+    let data = "4B7CXa6sgLXyWmXSzEGVaNjYDhbjjppN3cXk3FrrmC4kTYTCzjXEWTMluwXPuiupccBs7QWThNKzoqAZ0MHDC7bcalsYSI4HX6p9";
+    console.log(data);
+}
+test();
